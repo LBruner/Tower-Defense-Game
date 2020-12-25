e7881c51b6bbc9a966b7ef9af1cae2590633c159
Leandro <leandrobruner15@gmail.com>
2020-12-25 09:10:53 -0300

    Starting Pathfinding System

---
+++ Tower Defense/Assets/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+
+    [SerializeField] float delayTime = 1f;
+    void Start()
+    {
+        //StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        Debug.Log("Starting Patrol");
+        foreach (Waypoint waypoint in path)
+        {
+            Debug.Log("Starting Patrol" + "Visiting block: " + waypoint.transform.position);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(delayTime);
+        }
+        Debug.Log("Ending Patrol");
+    }
+    void Update()
+    {
+    }
+}
---
+++ Tower Defense/Assets/PathFinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    [SerializeField] Waypoint startPos, endPos;
+    void Start()
+    {
+        LoadBlocks();
+        ColorStartAndEnd();
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))
+                Debug.Log("Overlaping" + waypoint);
+            else
+            {
+                grid.Add(waypoint.GetGridPos(), waypoint);
+            }                
+        }
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startPos.SetTopColor(Color.green);
+        endPos.SetTopColor(Color.red);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
